536e82791bfcf2b968dc643b60e105e886c2bbe7
John Hendricks <Hendricksjf@g.cofc.edu>
2021-09-01 13:12:06 -0400

    completed HW 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,14 @@
+"""
+Name: John Hendricks
+chaos.py
+
+Problem: print "hello, world!" to the screen
+
+Certification of authenticity:
+I certify that this assignment is entirely my own work
+
+"""
+def main():
+    print('hello, world!')
+
+main()
